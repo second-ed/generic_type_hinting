6d3b404a57bbb3bbfd60f6b29c363e3feb598116
gazorpazorpius <edcussmusic@gmail.com>
2024-04-10 22:57:54 +0100

    first commit

---
+++ src/constants.py
@@ -0,0 +1,68 @@
+from typing import (
+    Collection,
+    Container,
+    Iterable,
+    Mapping,
+    Reversible,
+    Sequence,
+    Set,
+    Sized,
+)
+
+DUNDER_METHODS = {
+    '__and__',
+    '__contains__',
+    '__eq__',
+    '__ge__',
+    '__getitem__',
+    '__gt__',
+    '__iter__',
+    '__le__',
+    '__len__',
+    '__lt__',
+    '__ne__',
+    '__or__',
+    '__reversed__',
+    '__sub__',
+    '__xor__',
+    'count',
+    'get',
+    'index',
+    'items',
+    'keys',
+    'values'
+}
+
+ITERABLE = {
+    "__iter__"
+}
+
+SIZED = {
+    "__len__"
+}
+
+CONTAINER = {
+    "__contains__"
+}
+
+COLLECTION = ITERABLE | SIZED | CONTAINER
+
+REVERSIBLE = ITERABLE | {"__reversed__"}
+
+SEQUENCE = COLLECTION | REVERSIBLE | {"__getitem__", "index", "count"}
+
+MAPPING = COLLECTION | {"__getitem__", "__eq__", "__ne__", "get", "items", "keys", "values"}
+
+SET = COLLECTION | {"isdisjoint", "__le__", "__lt__", "__gt__", "__ge__", "__eq__", "__ne__", "__and__", "__or__", "__sub__", "__xor__"}
+
+
+TYPING_DUNDERS = (
+    (SET, Set),
+    (MAPPING, Mapping),
+    (SEQUENCE, Sequence),
+    (REVERSIBLE, Reversible),
+    (COLLECTION, Collection),
+    (CONTAINER, Container),
+    (SIZED, Sized),
+    (ITERABLE, Iterable)
+)
---
+++ src/generic_type_hinter.py
@@ -0,0 +1,25 @@
+
+
+from src.constants import DUNDER_METHODS, TYPING_DUNDERS
+
+
+def get_single_generic_type(obj):
+    has_methods = []
+
+    for method in DUNDER_METHODS:
+        if hasattr(obj, method):
+            has_methods.append(method)
+
+    for dunders, type_hint in TYPING_DUNDERS:
+        if dunders.issubset(has_methods):
+            return type_hint
+    return None
+
+
+def get_generic_type(objs):
+    type_hints = []
+    for obj in objs:
+        type_hints.append(get_single_generic_type(obj))
+
+    return type_hints
+

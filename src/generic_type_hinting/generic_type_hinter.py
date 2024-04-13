9617b6a1f3fb9dd5b57b8525432760489bc39333
gazorpazorpius <edcussmusic@gmail.com>
2024-04-13 10:50:43 +0100

    added analyse_group()

--- src/generic_type_hinting/generic_type_hinter.py
+++ src/generic_type_hinting/generic_type_hinter.py
@@ -1,9 +1,12 @@
 import attr
+from attr.validators import instance_of
 from generic_type_hinting.constants import DUNDER_METHODS, TYPING_DUNDERS
 
 
 @attr.define
 class GenericTypeHinter:
+    type_dict: dict = attr.ib(init=False, validator=[instance_of(dict)])
+
     def get_single_generic_type(self, obj):
         has_methods = []
 
@@ -23,7 +26,22 @@ class GenericTypeHinter:
 
         return type_hints
 
-    def get_group_type(self, objs):
+    def set_group_type(self, objs):
         types = self.get_generic_types(objs)
-        type_dict = {t: len(t.mro()) for t in types if t is not None}
-        return min(type_dict, key=type_dict.get)
+        self.type_dict = {t: t.mro() for t in types if t is not None}
+
+    def get_group_types(self):
+        common_elements = set(self.type_dict[next(iter(self.type_dict))])
+        for lst in self.type_dict.values():
+            common_elements.intersection_update(lst)
+
+        common_elements_in_order = [
+            elem for elem
+            in self.type_dict[next(iter(self.type_dict))]
+            if elem in common_elements
+        ]
+        return common_elements_in_order
+
+    def analyse_group(self, objs):
+        self.set_group_type(objs)
+        return self.get_group_types()

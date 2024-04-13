68ac9274a58eaf014fe789d9cd8a98368af8514a
gazorpazorpius <edcussmusic@gmail.com>
2024-04-13 22:11:58 +0100

    initial tests

--- src/generic_type_hinting/generic_type_hinter.py
+++ src/generic_type_hinting/generic_type_hinter.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import attr
 from attr.validators import instance_of
 from generic_type_hinting.constants import DUNDER_METHODS, TYPING_DUNDERS
@@ -6,8 +8,11 @@ from generic_type_hinting.constants import DUNDER_METHODS, TYPING_DUNDERS
 @attr.define
 class GenericTypeHinter:
     type_dict: dict = attr.ib(init=False, validator=[instance_of(dict)])
+    common_elems_ordered: list = attr.ib(
+        init=False, validator=[instance_of(list)]
+    )
 
-    def get_single_generic_type(self, obj):
+    def get_single_generic_type(self, obj: object) -> Optional[object]:
         has_methods = []
 
         for method in DUNDER_METHODS:
@@ -19,29 +24,28 @@ class GenericTypeHinter:
                 return type_hint
         return None
 
-    def get_generic_types(self, objs):
+    def get_generic_types(self, objs: list) -> list:
+        if not isinstance(objs, list):
+            raise TypeError(f"objs must be a list. Received {type(objs)}")
         type_hints = []
         for obj in objs:
             type_hints.append(self.get_single_generic_type(obj))
-
         return type_hints
 
-    def set_group_type(self, objs):
+    def analyse_group(self, objs: list) -> list:
+        if not isinstance(objs, list):
+            raise TypeError(f"objs must be a list. Received {type(objs)}")
+
         types = self.get_generic_types(objs)
         self.type_dict = {t: t.mro() for t in types if t is not None}
 
-    def get_group_types(self):
         common_elements = set(self.type_dict[next(iter(self.type_dict))])
         for lst in self.type_dict.values():
             common_elements.intersection_update(lst)
 
-        common_elements_in_order = [
-            elem for elem
-            in self.type_dict[next(iter(self.type_dict))]
+        self.common_elems_ordered = [
+            elem
+            for elem in self.type_dict[next(iter(self.type_dict))]
             if elem in common_elements
         ]
-        return common_elements_in_order
-
-    def analyse_group(self, objs):
-        self.set_group_type(objs)
-        return self.get_group_types()
+        return self.common_elems_ordered
---
+++ tests/test_generic_type_hinter.py
@@ -0,0 +1,142 @@
+import collections
+import typing
+
+import pytest
+from generic_type_hinting.generic_type_hinter import GenericTypeHinter
+
+
+@pytest.fixture
+def get_instance() -> GenericTypeHinter:
+    return GenericTypeHinter()
+
+
+def test_init(get_instance: GenericTypeHinter) -> None:
+    assert isinstance(get_instance, GenericTypeHinter)
+
+
+@pytest.mark.parametrize(
+    "objs, expected_result",
+    [
+        (
+            [{}],
+            [
+                collections.abc.Mapping,
+                collections.abc.Collection,
+                collections.abc.Sized,
+                collections.abc.Iterable,
+                collections.abc.Container,
+                object,
+            ],
+        ),
+        (
+            [set()],
+            [
+                collections.abc.Collection,
+                collections.abc.Sized,
+                collections.abc.Iterable,
+                collections.abc.Container,
+                object,
+            ],
+        ),
+        (
+            [[]],
+            [
+                collections.abc.Sequence,
+                collections.abc.Reversible,
+                collections.abc.Collection,
+                collections.abc.Sized,
+                collections.abc.Iterable,
+                collections.abc.Container,
+                object,
+            ],
+        ),
+        (
+            [{}, []],
+            [
+                collections.abc.Collection,
+                collections.abc.Sized,
+                collections.abc.Iterable,
+                collections.abc.Container,
+                object,
+            ],
+        ),
+        (
+            [set(), {}],
+            [
+                collections.abc.Collection,
+                collections.abc.Sized,
+                collections.abc.Iterable,
+                collections.abc.Container,
+                object,
+            ],
+        ),
+        (
+            [[], set()],
+            [
+                collections.abc.Collection,  # type: ignore
+                collections.abc.Sized,
+                collections.abc.Iterable,
+                collections.abc.Container,
+                object,
+            ],
+        ),
+    ],
+)
+def test_values_analyse_group(
+    get_instance: GenericTypeHinter, objs, expected_result
+) -> None:
+    get_instance.analyse_group(objs) == expected_result
+
+
+@pytest.mark.parametrize(
+    "objs",
+    [
+        ({"test": "case"}),
+        ({"test", "case"}),
+    ],
+)
+def test_types_analyse_group(get_instance: GenericTypeHinter, objs) -> None:
+    with pytest.raises(TypeError):
+        get_instance.analyse_group(objs)
+
+
+@pytest.mark.parametrize(
+    "objs, expected_result",
+    [
+        ([{}, []], [typing.Mapping, typing.Sequence]),
+        ([set(), {}], [typing.Collection, typing.Mapping]),
+        ([[], set()], [typing.Sequence, typing.Collection]),
+    ],
+)
+def test_values_get_generic_types(
+    get_instance: GenericTypeHinter, objs, expected_result
+) -> None:
+    get_instance.get_generic_types(objs) == expected_result
+
+
+@pytest.mark.parametrize(
+    "objs",
+    [
+        ({"test": "case"}),
+        ({"test", "case"}),
+    ],
+)
+def test_types_get_generic_types(
+    get_instance: GenericTypeHinter, objs
+) -> None:
+    with pytest.raises(TypeError):
+        get_instance.get_generic_types(objs)
+
+
+@pytest.mark.parametrize(
+    "obj, expected_result",
+    [
+        ([], typing.Sequence),
+        (set(), typing.Collection),
+        ({}, typing.Mapping),
+    ],
+)
+def test_values_get_single_generic_type(
+    get_instance: GenericTypeHinter, obj, expected_result
+) -> None:
+    get_instance.get_single_generic_type(obj) is None
